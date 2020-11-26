35ea35a940f42bf1e81b6faa648f07a6fa1b07fa
Py-Dude <62688535+Py-Dude@users.noreply.github.com>
2020-11-26 15:52:44 +0530

    Level 1 completed

--- CHANDF.py
+++ CHANDF.py
@@ -1,3 +1,135 @@
+def player1move():
+    s=input().split(":")
+    # Is that character alive
+    if s[0] in d1:
+        if(s[1] in "FLRB"):
+            
+            if(s[1]=="F"):
+                temp=d1[s[0]]
+                if(temp[1]>0 and temp[1]<=4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0],temp[1]-1]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+
+            if(s[1]=="B"):
+                temp=d1[s[0]]
+                if(temp[1]>=0 and temp[1]<4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0],temp[1]+1]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+
+            if(s[1]=="L"):
+                temp=d1[s[0]]
+                if(temp[0]>0 and temp[0]<=4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0]-1,temp[1]]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+
+            if(s[1]=="R"):
+                temp=d1[s[0]]
+                if(temp[0]>=0 and temp[0]<4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0]+1,temp[1]]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+        else:
+            print("Invalid Move")
+    else:
+        print("Invalid Move")
+
+def player2move():
+    s=input().split(":")
+    # Is that character alive
+    if s[0] in d1:
+        if(s[1] in "FLRB"):
+            
+            if(s[1]=="B"):
+                temp=d1[s[0]]
+                if(temp[1]>0 and temp[1]<=4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0],temp[1]-1]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+
+            if(s[1]=="F"):
+                temp=d1[s[0]]
+                if(temp[1]>=0 and temp[1]<4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0],temp[1]+1]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+
+            if(s[1]=="R"):
+                temp=d1[s[0]]
+                if(temp[0]>0 and temp[0]<=4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0]-1,temp[1]]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+
+            if(s[1]=="L"):
+                temp=d1[s[0]]
+                if(temp[0]>=0 and temp[0]<4):
+                    op=checkkill(temp,s[1])
+                    if(op!=-1):
+                        d1[s[0]]=d2[op]
+                        del(d2[op])
+                    else:
+                        d1[s[0]]=[temp[0]+1,temp[1]]
+                    board[temp[0]][temp[1]],board[d1[s[0]][0]][d1[s[0]][1]]=board[d1[s[0]][0]][d1[s[0]][1]],board[temp[0]][temp[1]]
+                    print(board)
+                else:
+                    print("Invalid Move")
+        else:
+            print("Invalid Move")
+    else:
+        print("Invalid Move")
+
 def checkinput(a):
     for j in a:
         if(j[0]=="P" and (j[1]=="1" or j[1]=="2" or j[1]=="3" or j[1]=="4" or j[1]=="5")):
@@ -6,17 +138,37 @@ def checkinput(a):
             return(-1)
     return(0)
 
-def checkkillF(temp):
-    for k in d1:
-        if(d1[k][0]==temp[0] and d1[k][1]==temp[1]-1):
-            return(k)
+def checkkill(temp,q):
+    for i in d2:
+        if(q=="F"):
+            if(d2[i][0]==temp[0] and d2[i][1]==temp[1]-1):
+                return(i)
+        elif(q=="B"):
+            if(d2[i][0]==temp[0] and d2[i][1]==temp[1]+1):
+                return(i)
+        elif(q=="L"):
+            if(d2[i][0]==temp[0]-1 and d2[i][1]==temp[1]):
+                return(i)
+        elif(q=="R"):
+            if(d2[i][0]==temp[0]+1 and d2[i][1]==temp[1]):
+                return(i)
     return(-1)
-def deadlock():    
 
-    
+def winner():
+    if(len(d1)==0):
+        print("Player 2 wins")
+        return(0)
+    if(len(d2)==0):
+        print("Player 1 wins")
+        return(0)
+    return(1)
 d1={"P1":[],"P2":[],"P3":[],"P4":[],"P5":[]}
+#Positions of player 1
 d2={"P1":[],"P2":[],"P3":[],"P4":[],"P5":[]}
+#Positions of player 2
 board=[["-" for k in range(0,5)]for j in range(0,5)]
+#Positions of board at any time
+
 a=input().split(",")
 while(if(checkinput(a)==-1)):
     print("Invalid input format")
@@ -25,6 +177,7 @@ for j in range(0,5):
     d1[a[j]]=[4,j]
     board[4][j]="A-"+a[j]
 print(board)
+
 b=input().split(",")
 while(if(checkinput(b)==-1)):
     print("Invalid input format")
@@ -33,23 +186,14 @@ for j in range(0,5):
     d2[b[j]]=[0,j]
     board[0][j]="B-"+b[j]
 print(board)
+
 k=0 #which player is moving 0-A || 1-B
 while(True):
     if(k==0):
-        s=input().split(":")
-        if s[0] in d1:
-            if(s[1] in "FLRB"):
-                if(s[1]=="F"):
-                    temp=d1[s[0]]
-                    if(temp[1]>0 and temp1<=4):
-                        if(checkkill(temp)==1):
-                            
-                        else:
-                            
-                        board[temp[0]][temp[1]]
-                    else:
-                        print("Invalid Move")
-            else:
-                print("Invalid Move")
-        else:
-            print("Invalid Move")
+        player1move(1)
+    else:
+        player2move(1)
+    if(winner()==0):
+        break
+
+        
